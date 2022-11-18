c0e2e8cda6173e7a5cb9cf5d168806e6b964ae06
wariormex <91165602+wariormex@users.noreply.github.com>
2022-11-18 16:18:28 -0600

    First Commit

---
+++ djangoRest/DjangoRest/DjangoRest/urls.py
@@ -0,0 +1,16 @@
+from django.conf import settings
+from django.conf.urls import include
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path
+from .views import HomeTemplateView, TestAuthView, LogoutViewEx
+from dj_rest_auth.views import LoginView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('test_auth/', TestAuthView.as_view(), name='test_auth', ),
+    path('rest-auth/logout/', LogoutViewEx.as_view(), name='rest_logout', ),
+    path('rest-auth/login/', LoginView.as_view(), name='rest_login', ),
+    path('', HomeTemplateView.as_view(), name='home', ),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
---
+++ djangoRest/DjangoRest/DjangoRest/views.py
@@ -0,0 +1,25 @@
+from django.views.generic import TemplateView
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import authentication, permissions
+
+from dj_rest_auth.views import LogoutView
+
+
+class HomeTemplateView(TemplateView):
+    template_name = 'home.html'
+
+
+class TestAuthView(APIView):
+    authentication_classes = (authentication.TokenAuthentication,)
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get(self, request, format=None):
+        return Response("Hello {0}!".format(request.user))
+    
+    def post(self, request, format=None):
+        return Response("Hello {0}! Posted!".format(request.user))
+
+
+class LogoutViewEx(LogoutView):
+    authentication_classes = (authentication.TokenAuthentication,)
